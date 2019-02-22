caff05b628b6e7a83fabf91d087e51d7d1263d45
stonezc <172494429@qq.com>
2019-02-22 17:57:38 +0800

    Update ingest.sh

--- ingest.sh
+++ ingest.sh
@@ -2,9 +2,9 @@
 
 FILES=/opt/nmap-xml/*.xml
 DATE=`date +%Y.%m.%d`
-curl -XDELETE "124.250.245.113:9200/nmap*"
+curl -XDELETE "localhost:9200/nmap*"
 for f in $FILES
 do
     echo "Processing $f file..."
-    python /opt/nmap-elk-sh/VulntoES.py -i $f -e es -r nmap -I nmap-$DATE
+    python /opt/nmap-elk-sh/VulntoES.py -i $f -e localhost -r nmap -I nmap-$DATE
 done
